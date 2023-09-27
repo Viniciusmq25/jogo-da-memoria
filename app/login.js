467872338441a28b3eac00ad48948b285f6115c8
Vinicius <viniciusquintian@gmail.com>
2023-09-27 17:39:32 -0300

    post

---
+++ app/login.js
@@ -0,0 +1,40 @@
+const noteInput = document.getElementById('nick')
+const noteForm = document.getElementById('form');
+const noteSubmit = document.getElementById('submit')
+const notes = document.getElementById("notes");
+
+let notesStorage = localStorage.getItem("note")
+  ? JSON.parse(localStorage.getItem("notes")) 
+  : []; 
+
+noteForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  notesStorage.push(noteInput.value);
+  localStorage.setItem("notes", JSON.stringify(notesStorage));
+  listBuilder(noteInput.value)
+  noteInput.value = "";
+
+  if(notesStorage.length > 10){
+    localStorage.removeItem("notes");
+    notes.innerHTML = ''
+    notesStorage = [];
+  }
+
+  redirecionar()
+})
+
+const listBuilder = (text) => {
+  const note = document.createElement("li");
+  note.innerHTML = text
+  notes.appendChild(note);
+};
+
+const getNotes = JSON.parse(localStorage.getItem("notes"));
+getNotes.forEach((note) => {
+  listBuilder(note);
+});
+
+
+function redirecionar(){
+  window.location.href = "jogo.html";
+}
